fd11d5f76109cb0069852846e3427438780d4b4a
VinayBorate <vinayborate121@gmail.com>
2025-03-27 19:36:42 +0530

    Multi_VideoFix_PlanCar_Fix

--- models/Plans.js
+++ models/Plans.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const planSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
+  plantitle: {type: String, required: true, unique: true},
   amount: { type: Number, required: true },
   features: { type: [String], default: [] },
 });
--- utils/seedPlans.js
+++ utils/seedPlans.js
@@ -1,18 +1,20 @@
 const Plan = require("../models/Plans");
 
 const plans = [
-  { name: "gold", amount: 100, features: ["24/7 support", "Unlimited access"] },
-  { name: "silver", amount: 50, features: ["Feature 2", "Extended access"] },
-  { name: "bronze", amount: 10, features: ["Feature 1", "Limited access"] },
+  { name: "Gold", plantitle: "GOLD", amount: 100, features: ["24/7 support", "Unlimited access"] },
+  { name: "Silver", plantitle: "SILVER",amount: 50, features: ["Feature 2", "Extended access"] },
+  { name: "Bronze", plantitle: "BRONZ",amount: 10, features: ["Feature 1", "Limited access"] },
 ];
 
 const seedPlans = async () => {
   try {
     for (const plan of plans) {
-      const existingPlan = await Plan.findOne({ name: plan.name });
+      const planNameLower = plan.name.toLowerCase(); // Convert name to lowercase
+      const existingPlan = await Plan.findOne({ name: planNameLower });
+
       if (!existingPlan) {
-        await Plan.create(plan);
-        console.log(`Added plan: ${plan.name}`);
+        await Plan.create({ ...plan, name: planNameLower });
+        console.log(`Added plan: ${planNameLower}`);
       }
     }
   } catch (error) {
